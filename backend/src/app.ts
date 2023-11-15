a1729ff945682aab12a95527e10fe807862b00e2
Victor Ng <thaivu.tcnh2@gmail.com>
2023-11-15 19:03:27 +1100

    added-boilerplate

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,10 +2,45 @@ import http from 'http';
 import url from 'url';
 import crypto from 'crypto';
 import querystring from 'querystring';
+import { client } from './config/database';
 
 const PORT: number = 3000;
 
 const server = http.createServer((req, res) => {
+    const pathUrl = url.parse(req.url || '', true);
+    const path = pathUrl.path;
+    const queryParams = pathUrl.query;
+
+    switch (path) {
+        // Handle login and create session
+        case '/login': {
+            if (req.method === 'POST') {
+                try {
+                    client.connect();
+                } catch (error) {
+                    console.error(`Unable to connect ${error}`);
+                    throw error;
+                } finally {
+                    client.end();
+                }
+            } else {
+                res.writeHead(405, { 'Content-type': 'application/json' });
+                res.end(JSON.stringify({ message: 'Invalid request' }));
+            }
+        }
+        case '/logout': {
+        }
+        case '/resigster': {
+        }
+        case '/resigster': {
+        }
+
+        default: {
+            res.writeHead(200, { 'Content-type': 'application.json' });
+            res.end('Default entry');
+        }
+    }
+
     res.writeHead(200, { 'Content-type': 'text/plain' });
     res.end('End here');
 });
---
+++ backend/src/config/database.ts
@@ -0,0 +1,17 @@
+import { Client } from 'pg';
+
+const user = process.env.USER_DEV;
+const host = process.env.HOST_DEV;
+const database = process.env.UNI_DEV;
+const password = process.env.PASSWORD_DEV;
+const port = 5432;
+
+const client = new Client({
+    user,
+    host,
+    database,
+    password,
+    port,
+});
+
+export { client };
